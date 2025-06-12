4fbb5438f9fe80c22e76e3e8a7a0f6108c8d0ba6
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-13 04:37:39 +0800

    style: full width skeleton loader

--- components/skeleton-loading.tsx
+++ components/skeleton-loading.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export function SkeletonLoader({ msg }: { msg: string }) {
     return (
-        <div className="flex flex-col items-center space-x-4 ">
+        <div className="w-full h-full flex flex-col items-center space-x-4 ">
             <h1 className="font-semibold text-2xl mb-3 text-blue-600">{msg}</h1>
             <div className="space-y-2">
                 <Skeleton className="h-6 w-[550px] !bg-blue-300" />
