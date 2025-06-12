29d715d5d95deafd475ab0508e56949229ecb484
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-13 04:29:00 +0800

    feat: delete essay

---
+++ app/api/essay/[id]/route.ts
@@ -0,0 +1,20 @@
+import { db } from '@/database'
+import { essay } from '@/database/schema'
+import { eq, sql } from 'drizzle-orm'
+import { NextRequest, NextResponse } from 'next/server'
+
+export async function DELETE(request: NextRequest, { params }: { params: { id: string } }) {
+    const essayID = params.id
+    try {
+        await db.transaction(async (tx) => {
+            await tx.delete(essay).where(eq(essay.id, essayID))
+            await tx.execute(sql`DELETE FROM essay_evaluations WHERE essay_id = ${essayID}`)
+            await tx.execute(sql`DELETE FROM essay_summaries WHERE essay_id = ${essayID}`)
+        })
+
+        return NextResponse.json({ success: true }, { status: 200 })
+    } catch (err) {
+        console.error('Failed to delete rubric:', err)
+        return NextResponse.json({ success: false, message: 'Server error' }, { status: 500 })
+    }
+}
--- app/essays/columns.tsx
+++ app/essays/columns.tsx
@@ -14,6 +14,10 @@ import { PrimaryButton } from '@/components/ui/primary-button'
 import { EssayWithEvalSummary } from '@/types/essay'
 
 import { Loader2 } from 'lucide-react'
+import { DialogDescription } from '@radix-ui/react-dialog'
+import DeleteEssay from '@/components/essays/delete-essay'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { toast } from 'sonner'
 
 export type Essay = {
     id: string
@@ -70,7 +74,7 @@ export const columns: ColumnDef<EssayWithEvalSummary>[] = [
                         <Loader2 className="w-4 h-4 animate-spin text-yellow-600" />
                     )}
                     <Badge className={badgeClass} variant="outline">
-                        {status === 'pending' ? "EVALUATING" : status.toUpperCase()}
+                        {status === 'pending' ? 'EVALUATING' : status.toUpperCase()}
                     </Badge>
                 </div>
             )
@@ -81,77 +85,140 @@ export const columns: ColumnDef<EssayWithEvalSummary>[] = [
         header: 'Actions',
         cell: ({ row }) => {
             const essay = row.original
-            const [open, setOpen] = useState<boolean>(false)
-            const isPending = essay.status === 'pending'
+            const isGradingStatusPending = essay.status === 'pending'
+
+            const queryClient = useQueryClient()
+            const [openDeleteDialog, setOpenDeleteDialog] = useState<boolean>(false)
+
+            const closeDialog = () => setOpenDeleteDialog(false)
+
+            const deleteEssayMutation = useMutation({
+                mutationFn: async (essayID: string) => {
+                    const res = await fetch(`/api/essay/${essayID}`, {
+                        method: 'DELETE',
+                    })
+                    const data = await res.json()
+
+                    if (!res.ok) {
+                        throw new Error(data?.message || 'Failed to delete essay')
+                    }
+
+                    return data
+                },
+
+                onSuccess: () => {
+                    queryClient.invalidateQueries({ queryKey: ['essays'] })
+                    toast.success('Essay deleted successfully!')
+                    closeDialog()
+                },
+
+                onError: (err: any) => {
+                    console.error('Error deleting essay:', err)
+                    toast.error('Failed to delete essay, please try again.')
+                    closeDialog()
+                },
+            })
+
+            const handleDeleteEssay = () => {
+                deleteEssayMutation.mutate(essay.id)
+            }
 
             return (
-                <Dialog open={open} onOpenChange={setOpen}>
-                    <DialogTrigger asChild>
-                        <PrimaryButton
-                            color="blue"
-                            size="sm"
-                            disabled={isPending}
-                            className={isPending ? 'opacity-50 cursor-not-allowed' : ''}
-                        >
-                            Preview
-                        </PrimaryButton>
-                    </DialogTrigger>
-                    <DialogContent className="md:!max-w-3xl lg:!max-w-4xl">
-                        <DialogHeader>
-                            <DialogTitle>Evaluations for "{essay.name}"</DialogTitle>
-                        </DialogHeader>
+                <div className="flex items-center gap-2">
+                    <Dialog>
+                        <DialogTrigger asChild>
+                            <PrimaryButton
+                                color="blue"
+                                size="sm"
+                                disabled={isGradingStatusPending}
+                                className={
+                                    isGradingStatusPending ? 'opacity-50 cursor-not-allowed' : ''
+                                }
+                            >
+                                Preview
+                            </PrimaryButton>
+                        </DialogTrigger>
+                        <DialogContent className="md:!max-w-3xl lg:!max-w-4xl">
+                            <DialogHeader>
+                                <DialogTitle>Evaluations for "{essay.name}"</DialogTitle>
+                            </DialogHeader>
 
-                        <div className="space-y-6 max-h-[70vh] overflow-y-auto px-1">
-                            {essay.evaluations?.length ? (
-                                <>
-                                    {essay.evaluations.map((evalItem, index) => (
-                                        <div
-                                            key={index}
-                                            className="bg-white border rounded-lg p-4 shadow-sm space-y-3"
-                                        >
-                                            <div className="flex items-center justify-between">
-                                                <h3 className="text-md font-semibold">
-                                                    {evalItem.criterion} ({evalItem.score}/
-                                                    {evalItem.max_score})
-                                                </h3>
-                                                {/* Optional Edit/Delete buttons here */}
+                            <div className="space-y-6 max-h-[70vh] overflow-y-auto px-1">
+                                {essay.evaluations?.length ? (
+                                    <>
+                                        {essay.evaluations.map((evalItem, index) => (
+                                            <div
+                                                key={index}
+                                                className="bg-white border rounded-lg p-4 shadow-sm space-y-3"
+                                            >
+                                                <div className="flex items-center justify-between">
+                                                    <h3 className="text-md font-semibold">
+                                                        {evalItem.criterion} ({evalItem.score}/
+                                                        {evalItem.max_score})
+                                                    </h3>
+                                                    {/* Optional Edit/Delete buttons here */}
+                                                </div>
+
+                                                <div className="text-sm text-gray-600">
+                                                    <p>{evalItem.reason}</p>
+                                                </div>
+
+                                                {evalItem.suggestion && (
+                                                    <div className="bg-blue-50 border-l-4 border-blue-400 p-3 text-sm rounded-md">
+                                                        <strong className="text-blue-700">
+                                                            Suggestion:
+                                                        </strong>{' '}
+                                                        {evalItem.suggestion}
+                                                    </div>
+                                                )}
                                             </div>
+                                        ))}
 
-                                            <div className="text-sm text-gray-600">
-                                                <p>{evalItem.reason}</p>
-                                            </div>
+                                        {/* Overall Grade Section */}
+                                        {essay.summary &&
+                                            essay.summary.overall_feedback &&
+                                            essay.summary.total_score &&
+                                            essay.summary.max_total_score && (
+                                                <div className="bg-blue-600 text-white border rounded-lg p-4 shadow-sm space-y-3">
+                                                    <h1 className="text-xl font-semibold">
+                                                        Overall Evaluation
+                                                    </h1>
 
-                                            {evalItem.suggestion && (
-                                                <div className="bg-blue-50 border-l-4 border-blue-400 p-3 text-sm rounded-md">
-                                                    <strong className="text-blue-700">
-                                                        Suggestion:
-                                                    </strong>{' '}
-                                                    {evalItem.suggestion}
+                                                    <p className="text-md mb-2">
+                                                        Total Score: {essay.summary.total_score} /{' '}
+                                                        {essay.summary.max_total_score}{' '}
+                                                        {/* PERCENTAGE */}
+                                                        {(
+                                                            (essay.summary.total_score /
+                                                                essay.summary.max_total_score) *
+                                                            100
+                                                        ).toFixed(1)}
+                                                        %
+                                                    </p>
+
+                                                    <p className="text-md">
+                                                        Feedback: {essay.summary.overall_feedback}
+                                                    </p>
                                                 </div>
                                             )}
-                                        </div>
-                                    ))}
+                                    </>
+                                ) : (
+                                    <p className="text-sm text-muted-foreground">
+                                        No evaluations available.
+                                    </p>
+                                )}
+                            </div>
+                        </DialogContent>
+                    </Dialog>
 
-                                    {/* Overall Grade Section */}
-                                    {essay.summary && essay.summary.overall_feedback && (
-                                        <div className="bg-blue-600 text-white border rounded-lg p-4 shadow-sm space-y-3">
-                                            <h1 className="text-xl font-semibold">
-                                                Overall Feedback
-                                            </h1>
-                                            <p className="text-md">
-                                                {essay.summary.overall_feedback}
-                                            </p>
-                                        </div>
-                                    )}
-                                </>
-                            ) : (
-                                <p className="text-sm text-muted-foreground">
-                                    No evaluations available.
-                                </p>
-                            )}
-                        </div>
-                    </DialogContent>
-                </Dialog>
+                    <DeleteEssay
+                        onDelete={handleDeleteEssay}
+                        isPending={deleteEssayMutation.isPending}
+                        isGraded={!isGradingStatusPending}
+                        openDialog={openDeleteDialog}
+                        setOpenDialog={setOpenDeleteDialog}
+                    />
+                </div>
             )
         },
     },
