1e8e388c517a0e8bd66fd97f3ce27e626fdf6afe
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-14 12:52:40 +0800

    adjust loader size

--- components/skeleton-loading.tsx
+++ components/skeleton-loading.tsx
@@ -4,10 +4,10 @@ export function SkeletonLoader({ msg }: { msg: string }) {
     return (
         <div className="w-full h-full flex flex-col items-center space-x-4 ">
             <h1 className="font-semibold text-2xl mb-3 text-blue-600">{msg}</h1>
-            <div className="space-y-2">
-                <Skeleton className="h-6 w-[550px] !bg-blue-300" />
-                <Skeleton className="h-6 w-[350px] !bg-blue-300" />
-                <Skeleton className="h-6 w-[470px] !bg-blue-300" />
+            <div className="w-full space-y-2">
+                <Skeleton className="h-6 w-full !bg-blue-300" />
+                <Skeleton className="h-6 w-[60%] !bg-blue-300" />
+                <Skeleton className="h-6 w-[80%] !bg-blue-300" />
             </div>
         </div>
     )
