b01a45967700d401d01e6f250caf91b1b2138fbc
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-22 13:27:30 +0800

    change secret to use the default

--- lib/next-auth/config.ts
+++ lib/next-auth/config.ts
@@ -2,7 +2,7 @@ import { NextAuthConfig } from 'next-auth'
 import Google from 'next-auth/providers/google'
 
 export const nextAuthConfig: NextAuthConfig = {
-    secret: process.env.NEXTAUTH_SECRET,
+    secret: process.env.AUTH_SECRET,
     providers: [
         Google({
             clientId: process.env.AUTH_GOOGLE_ID,
