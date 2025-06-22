177b3e20c0c6b690fe4b01151f5744cc61e9d353
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-22 13:20:40 +0800

    redeploy

--- database/index.ts
+++ database/index.ts
@@ -2,4 +2,4 @@ import { drizzle } from 'drizzle-orm/node-postgres'
 if (process.env.NODE_ENV !== 'production') {
     await import('dotenv/config')
 }
-export const db = drizzle(process.env.DATABASE_URL!)
+export const db = drizzle(process.env.DATABASE_URL!) 
