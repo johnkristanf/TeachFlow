621d1e7f9f9457f2a0d8d3c0bcba4e18708767dd
John Kristan Torremocha <johnkristan01@gmail.com>
2025-06-29 07:02:17 +0800

    refactor authentication schema

--- drizzle/0005_sloppy_stark_industries.sql
+++ drizzle/0005_sloppy_stark_industries.sql
@@ -1,24 +1,37 @@
-CREATE TABLE "accounts" (
-	"id" text PRIMARY KEY NOT NULL,
-	"user_id" text,
+CREATE TABLE "account" (
+	"userId" text,
 	"type" text NOT NULL,
 	"provider" text NOT NULL,
-	"provider_account_id" text NOT NULL,
-	"created_at" timestamp DEFAULT now()
+	"providerAccountId" text NOT NULL,
+	"refresh_token" text,
+	"access_token" text,
+	"expires_at" integer,
+	"token_type" text,
+	"scope" text,
+	"id_token" text,
+	"session_state" text
+);
+--> statement-breakpoint
+CREATE TABLE "verificationToken" (
+	"identifier" text NOT NULL,
+	"token" text NOT NULL,
+	"expires" timestamp NOT NULL
+);
+
+CREATE TABLE "user" (
+	"id" text PRIMARY KEY NOT NULL,
+	"email" text,
+	"name" text,
+	"image" text,
+	"emailVerified" timestamp,
+	CONSTRAINT "user_email_unique" UNIQUE("email")
+);
+
+CREATE TABLE "session" (
+	"sessionToken" text PRIMARY KEY NOT NULL,
+	"userId" text NOT NULL,
+	"expires" timestamp NOT NULL
 );
 
 --> statement-breakpoint
-CREATE TABLE "users" (
-	"id" text PRIMARY KEY NOT NULL,
-	"email" text NOT NULL,
-	"name" text,
-	"image" text,
-	"email_verified" timestamp,
-	"password" text,
-	"role" text DEFAULT 'USER',
-	"created_at" timestamp DEFAULT now(),
-	"updated_at" timestamp DEFAULT now(),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
---> statement-breakpoint
-ALTER TABLE "accounts" ADD CONSTRAINT "accounts_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE "account" ADD CONSTRAINT "account_user_id_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
