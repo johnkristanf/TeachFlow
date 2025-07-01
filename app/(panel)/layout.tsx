2b6118dafe3db9d7cf29f4770831f41e7b85fd2e
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-01 15:06:12 +0800

    removed unfinished panel dropdown

--- app/(panel)/layout.tsx
+++ app/(panel)/layout.tsx
@@ -83,7 +83,7 @@ export default async function PanelLayout({
                                             align="end"
                                             sideOffset={4}
                                         >
-                                            <DropdownMenuGroup>
+                                            {/* <DropdownMenuGroup>
                                                 <DropdownMenuItem>
                                                     <Sparkles />
                                                     Upgrade to Pro
@@ -103,7 +103,7 @@ export default async function PanelLayout({
                                                     <Bell />
                                                     Notifications
                                                 </DropdownMenuItem>
-                                            </DropdownMenuGroup>
+                                            </DropdownMenuGroup> */}
                                             <DropdownMenuSeparator />
 
                                             {/* LOGOUT MENU */}
