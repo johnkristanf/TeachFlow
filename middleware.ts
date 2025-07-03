88f9010fbffd23638b9c5eb6905d15f5f7f165a5
John Kristan Torremocha <johnkristan01@gmail.com>
2025-07-03 08:46:09 +0800

    simplified middleware check

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,7 @@
 import { NextRequest } from 'next/server'
 
-export default function middleware(request: NextRequest) {
-    // console.log('All cookies:', request.cookies.getAll())
+export default function middleware(request: NextRequest) {}
 
-    const devSessionCookie = request.cookies.get('authjs.session-token')
-    const prodSessionCookie = request.cookies.get('__Secure-authjs.session-token')
-
-    const isLoggedIn = !!(devSessionCookie?.value || prodSessionCookie?.value)
-    const isAuthPage = request.nextUrl.pathname.startsWith('/auth')
-
-    const publicRoutes = ['/', '/pricing', '/contact-us']
-    const protectedRoutes = ['/essays', '/classes', '/feedback', '/rubrics', '/quiz/generator'] // add more as needed
-
-    const isProtectedRoute = protectedRoutes.some((route) =>
-        request.nextUrl.pathname.startsWith(route)
-    )
-    const isPublicRoute = publicRoutes.some((route) => request.nextUrl.pathname === route)
-
-    // ✅ Redirect logged-in users away from auth pages & public routes like landing
-    if (isLoggedIn && (isAuthPage || isPublicRoute)) {
-        return Response.redirect(new URL('/essays', request.nextUrl))
-    }
-
-    // Redirect non-logged-in users to sign-in page for protected routes
-    if (!isLoggedIn && isProtectedRoute) {
-        return Response.redirect(new URL('/auth/signin', request.nextUrl))
-    }
+export const config = {
+    matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 }
